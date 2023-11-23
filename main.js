c5a4c9299174441aa4bced0f9f8805c34db333e5
Breno Costa <brenosofts@gmail.com>
2023-11-23 20:07:56 -0300

    'update'

---
+++ main.js
@@ -0,0 +1,4 @@
+document.querySelector('#menu-btn').addEventListener('click', function () {
+    document.querySelector('#menu-site').classList.toggle("active")
+    document.querySelector('#menu-icon').classList.toggle('active')
+})

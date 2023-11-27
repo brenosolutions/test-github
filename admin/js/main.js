27470c452311f2d5b6dcbeb2992e1fe74cd4b009
Breno Costa <brenosofts@gmail.com>
2023-11-27 12:49:31 -0300

    done

--- admin/js/main.js
+++ admin/js/main.js
@@ -48,5 +48,16 @@ function closeDropdownAction() {
 }
 
 
-
-
+// dorpdown sidebar
+var dropdownSidebar = document.querySelectorAll('.dropdown-btn')
+for (let i = 0; i < dropdownSidebar.length; i++) {
+    dropdownSidebar[i].addEventListener("click", function () {
+        this.classList.toggle("active")
+        var dropdownContent = this.nextElementSibling
+        if (dropdownContent.style.display === "block") {
+            dropdownContent.style.display = "none"
+        } else {
+            dropdownContent.style.display = "block"
+        }
+    })
+}
